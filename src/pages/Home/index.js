ad46d18454e512a920c56c415d336171421d61d8
GersoZz <37023982+GersoZz@users.noreply.github.com>
2024-05-02 10:37:19 -0500

    [Added] Search Form*

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,41 +1,29 @@
-import React, { useState } from "react";
+import React, { useCallback } from "react";
 import { useLocation } from "wouter";
 
 import ListOfGifs from "components/ListOfGifs/ListOfGifs";
 import { useGifs } from "hooks/useGifs";
 import TrendingSearches from "components/TrendingSearches";
+import SearchForm from "components/SearchForm";
 
 export default function Home() {
-  const [keyword, setKeyword] = useState("");
   const [path, pushLocation] = useLocation();
   const { loading, gifs } = useGifs();
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
+  const handleSubmit = useCallback(({ keyword }) => {
     //navegar a otra ruta
     pushLocation(`/search/${keyword}`);
-  };
-
-  const handleChange = (evt) => {
-    setKeyword(evt.target.value);
-  };
+  }, [pushLocation]);
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input
-          onChange={handleChange}
-          type="text"
-          value={keyword}
-          placeholder="Search a gif here ..."
-        />
-      </form>
+      <SearchForm onSubmit={handleSubmit} />
 
       <div className="App-main">
         <div className="App-results">
           <h3 className="App-title">Última búsqueda</h3>
-          <ListOfGifs gifs={gifs} />{/* Aqui deberia ir el loading */}
+          <ListOfGifs gifs={gifs} />
+          {/* Aqui deberia ir el loading */}
         </div>
         <div className="App-category">
           <TrendingSearches />
